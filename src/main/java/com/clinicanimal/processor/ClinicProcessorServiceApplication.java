cba9fbccbd281176fc9a0e8058dc5516dd6869c4
Carlos Berdejo <carlosberdejopar@gmail.com>
2026-02-21 00:22:26 -0500

    first commit

---
+++ src/main/java/com/clinicanimal/processor/ClinicProcessorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.clinicanimal.processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicProcessorServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClinicProcessorServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clinicanimal/processor/ClinicProcessorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinicanimal.processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicProcessorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
